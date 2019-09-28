0188473ff335c989310b5d808ec67594e560fac5
霸气千秋 <minbaby.zhang@gmail.com>
2019-09-28 23:29:20 +0800

    code

--- app/IM/Handler/Impl/NotFoundHandler.php
+++ app/IM/Handler/Impl/NotFoundHandler.php
@@ -3,14 +3,15 @@
 namespace App\IM\Handler\Impl;
 
 use App\IM\Handler\AbstractHandler;
+use App\IM\Handler\CodeEnum;
 use Swoole\Server;
 use Swoole\WebSocket\Frame;
 
-class NotSupportedHandler extends AbstractHandler
+class NotFoundHandler extends AbstractHandler
 {
     public function handler(Server $server, Frame $frame, $decodeData)
     {
-        $server->push($frame->fd, $this->packet->pack(['op' => 'not_supported']));
+        $server->push($frame->fd, $this->packet->pack(['op' => CodeEnum::OP_NOT_FOUND]));
         $this->push($server, $frame, []);
     }
 }
